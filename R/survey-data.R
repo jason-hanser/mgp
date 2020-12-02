5dec8a6ffbc424152a4050bbab66ea82ede64ecf
jason-hanser <hanser-jason@gmail.com>
2020-12-02 09:18:10 -0500

    added .Rdata file

---
+++ R/survey-data.R
@@ -0,0 +1,15 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+
+"survey"
